e2e2c53a82ee1b176b49107bfc0656ddd679ac02
AbdulrahmanMasoud <abdulrahman.masoud.official@gmial.com>
2023-02-10 00:36:18 +0200

    Setup file straucer

---
+++ scripts/content.js
@@ -0,0 +1,5 @@
+Array.from(document.getElementsByClassName('x193iq5w xeuugli x13faqbe x1vvkbs x10flsy6 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1tu3fi x3x7a5m x1lkfr7t x1lbecb7 x1s688f xzsf02u')).forEach((element, index)=>{
+    element.click();
+    console.log("Clicked");
+});
+
